5f28a81be398bb016cfe14658a6a35d5e6c17ffd
RedsTom <tom.frefre.13@gmail.com>
2025-01-11 18:10:39 +0100

    init(): Add spring boot, lombok and necessary libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,46 @@
 plugins {
-    id("java")
+    java
+
+    id("com.diffplug.spotless") version "7.0.0.BETA2"
+
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "fr.redstom"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 repositories {
     mavenCentral()
+    maven("https://maven.scijava.org/content/repositories/public/")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("club.minnced:java-discord-rpc:1.3.6")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
-tasks.test {
-    useJUnitPlatform()
+spotless {
+    java {
+        googleJavaFormat()
+            .reorderImports(true)
+            .formatJavadoc(true)
+            .reflowLongStrings()
+            .aosp()
+    }
 }
